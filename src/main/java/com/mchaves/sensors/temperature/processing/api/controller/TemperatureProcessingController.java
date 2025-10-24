122592297f22d4eaa17e30a1def202d7fb5450cc
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2025-10-23 21:28:18 -0300

    develop: Configuração de envio de dados para RabbitMQ
    Configuração de envio de dados para RabbitMQ

--- src/main/java/com/mchaves/sensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/mchaves/sensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -1,24 +1,36 @@
 package com.mchaves.sensors.temperature.processing.api.controller;
 
-import com.mchaves.sensors.temperature.processing.api.model.TemperatureLogOutput;
-import com.mchaves.sensors.temperature.processing.common.IdGenerator;
-import io.hypersistence.tsid.TSID;
-import lombok.extern.slf4j.Slf4j;
+import static com.mchaves.sensors.temperature.processing.infrastructure.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
+
+import java.time.OffsetDateTime;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.awt.*;
-import java.time.OffsetDateTime;
+import com.mchaves.sensors.temperature.processing.api.model.TemperatureLogOutput;
+import com.mchaves.sensors.temperature.processing.common.IdGenerator;
+
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/temperatures/data")
 @Slf4j
+@RequiredArgsConstructor
 public class TemperatureProcessingController {
 
+    private final RabbitTemplate rabbitTemplate;
+
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
-    public void data(@PathVariable TSID sensorId, @RequestBody String input) {
+    public void data(@PathVariable("sensorId") TSID sensorId, @RequestBody String input) {
         if (input == null || input.isBlank()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
@@ -39,5 +51,10 @@ public class TemperatureProcessingController {
                 .build();
 
         log.info(logOutput.toString());
+
+        String exchange = FANOUT_EXCHANGE_NAME;
+        String routingKey = "";
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, logOutput);
     }
 }
--- src/main/java/com/mchaves/sensors/temperature/processing/infrastructure/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/mchaves/sensors/temperature/processing/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -4,11 +4,21 @@ import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Configuration
 public class RabbitMQConfig {
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    public static final String FANOUT_EXCHANGE_NAME = "temperature-processing.temperature-received.v1.e";
 
     @Bean
     public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
@@ -16,8 +26,8 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public FanoutExchange exchange(){
-        return ExchangeBuilder.fanoutExchange("temperature-processing.temperature-received.v1.e").build();
+    public FanoutExchange exchange() {
+        return ExchangeBuilder.fanoutExchange(FANOUT_EXCHANGE_NAME).build();
     }
 
 }
